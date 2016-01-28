a04bf2cd2892fce6ca37f2b4da7af9dee8889f59
Jean-Baptiste Lenglet <jbaptiste.lenglet+git@gmail.com>
2016-01-28 11:29:37 +0100

    Added database config and how to start it.

--- config.js
+++ config.js
@@ -1,13 +1,24 @@
 const fs = require('fs');
 
-const config = {
+module.exports = {
   server: {
     port: 8000,
     https: {
       key: fs.readFileSync('certs/webflux.key'),
       cert: fs.readFileSync('certs/webflux.crt')
     }
+  },
+  env: "dev",
+  db: {
+    dev: {
+      protocol: 'mongodb',
+      slashes: true,
+      hostname: 'jblenglet.org.uk',
+      port: 28000,
+      pathname: 'webflux'
+    },
+    prod: {
+
+    }
   }
 };
-
-module.exports = config;
