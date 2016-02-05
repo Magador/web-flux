85ae88411ded5028b995ff3ee96a08d0c543a419
Jean-Baptiste Lenglet <jbaptiste.lenglet+git@gmail.com>
2016-02-05 14:51:25 +0100

    AAAAAAAAAAAAAAAAA

--- public/scripts/feeds.js
+++ public/scripts/feeds.js
@@ -10,3 +10,87 @@ signout.addEventListener('click', function (e) {
     })
     .go();
 });
+
+var main = document.querySelector('main');
+
+var addFeed = document.querySelector("#addFeed");
+var addFeedButton = document.querySelector("#addFeedButton");
+addFeedButton.addEventListener('click', function (e) {
+  e.stopPropagation();
+
+  var xhr = new XMLHttpRequest();
+  xhr.open('get', '/api/feeds?url=' + addFeed.value);
+  xhr.addEventListener('load', function (xhr) {
+    var div = document.createElement('div');
+    div.innerHTML = xhr.target.responseText;
+    main.appendChild(populate(div.firstElementChild));
+  });
+  xhr.send();
+});
+
+function populate(node) {
+  var section = document.createElement('section');
+  var channel = node.firstElementChild;
+  var title = channel.querySelector('title');
+  var link = channel.innerHTML.match(/<link>([^<]*)/i)[1];
+
+  var h2 = document.createElement('h2');
+  h2.innerHTML = '<a href="'+ link +'">'+ (title ? title.textContent: link) +'</a>';
+  section.appendChild(h2);
+
+  var toggleFeedButton = document.createElement('button');
+  toggleFeedButton.textContent = "Hide feed";
+  toggleFeedButton.addEventListener('click', toggleFeed.bind(toggleFeedButton));
+  section.appendChild(toggleFeedButton);
+
+  var removeFeedButton = document.createElement('button');
+  removeFeedButton.textContent = "Remove feed";
+  removeFeedButton.addEventListener('click', removeFeed.bind(removeFeedButton));
+  section.appendChild(removeFeedButton);
+
+  var articles = populateItems(channel.querySelectorAll('item'));
+  articles.forEach(function (article) {
+    section.appendChild(article);
+  })
+  return section;
+}
+
+function populateItems(items) {
+  var articles = [];
+  items.forEach(function (item) {
+    var article = document.createElement('article'),
+        title = item.querySelector('title'),
+        description = item.querySelector('description').textContent,
+        link = item.innerHTML.match(/<link>([^<]*)/i)[1];
+    var h3 = document.createElement('h3'),
+        div = document.createElement('div');
+
+    h3.innerHTML = '<a href="'+ link +'">'+ (title ? title.textContent: link) +'</a>';
+    article.appendChild(h3);
+
+    div.innerHTML = description;
+    article.appendChild(div);
+    articles.push(article);
+    article.classList.toggle('item');
+  });
+  return articles;
+}
+
+function toggleFeed(e) {
+  e.stopPropagation();
+  e.preventDefault();
+
+  if(this.classList.contains('hidden')) {
+    this.textContent = "Hide feed";
+  } else {
+    this.textContent = "Show feed";
+  };
+  this.classList.toggle('hidden');
+}
+
+function removeFeed(e) {
+  e.stopPropagation();
+  e.preventDefault();
+
+  this.parentElement.parentElement.removeChild(this.parentElement);
+}
--- routes/api.js
+++ routes/api.js
@@ -1,19 +1,28 @@
 "use strict";
 
 const express = require('express');
+const http = require('http');
+const request = require('request');
 const router = express.Router();
 
 const User = require('../models/user');
 
 router.get('/checkname/:username', checkName);
-router.all('*', requireAuth, loadUser);
 router.get('/feeds', getFeeds);
+// router.all('*', requireAuth, loadUser);
 
 module.exports = router;
 
 
-function getFeeds(res, req, next) {
-  
+function getFeeds(req, res) {
+  request(req.query.url, function (err, resp, body) {
+    if(err) {
+      res.setHeader('content-type', 'application/json');
+      res.status(500);
+      return res.end(JSON.stringify({"status":"error","status_message":"Query was unsuccessful"}));
+    }
+    res.send(body);
+  })
 }
 
 function checkName(req, res, next) {
